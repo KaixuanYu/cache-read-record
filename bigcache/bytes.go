adbc887dd1ad985e0dd1a477433940f234dff031
yukaixuan <2358685567@qq.com>
2020-11-14 23:32:33 +0800

    bigcache encoding.go 源码阅读

--- bigcache/bytes.go
+++ bigcache/bytes.go
@@ -8,7 +8,7 @@ import (
 )
 
 func bytesToString(b []byte) string {
-	// 这跟 string(b) 有啥区别？
+	// 这跟 string(b) 有啥区别？只是指针的转换，没有内存的拷贝。如果直接string(b)会重新开辟内存。然后重新拷贝过去。
 	bytesHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	strHeader := reflect.StringHeader{Data: bytesHeader.Data, Len: bytesHeader.Len}
 	return *(*string)(unsafe.Pointer(&strHeader))
